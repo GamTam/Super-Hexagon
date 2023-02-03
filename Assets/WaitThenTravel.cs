53a46692cdc65a6b63a170818c3ec83c47b86c34
Mabel <luccadacambra@gmail.com>
2023-02-02 20:52:03 -0500

    Minor Changes

---
+++ Assets/WaitThenTravel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitThenTravel : MonoBehaviour
+{
+    [SerializeField] private Vector3 _destination;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2);
+        
+        float movementDuration = 3;
+        float timeElapsed = 0;
+            
+        while (timeElapsed < movementDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            gameObject.transform.localPosition = Vector3.Lerp(gameObject.transform.localPosition, _destination, Time.deltaTime * 5);
+                    
+            yield return null;
+        }
+        
+        gameObject.transform.localPosition = _destination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
